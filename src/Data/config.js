d1705b89ccf8f5fa796bca330d59928d9c868ead
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2021-05-11 12:07:35 +0900

    Update config.js

--- src/Data/config.js
+++ src/Data/config.js
@@ -5,9 +5,9 @@ module.exports = {
   img: 'https://avatars.githubusercontent.com/u/57896501?v=4',
   github: 'https://github.com/kangdongmandoo',
   instagram: 'https://www.instagram.com/r.alsrkd/',
-  doing: ['🎮 게임하기', '🎈 놀기'],
+  doing: ['🎮게임하기', '🎈놀기'],
   team: {
     name: 'ten-humans',
     homepage: 'https://github.com/ten-humans',
   },
-}
+};

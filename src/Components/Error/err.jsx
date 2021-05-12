271f5bd7e62d891eb1e70aff136ce1efd1c0d03f
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2021-05-12 13:20:13 +0900

    Update err.jsx

--- src/Components/Error/err.jsx
+++ src/Components/Error/err.jsx
@@ -5,10 +5,21 @@ export default function Err(props) {
     textAlign: 'center',
     marginTop: '5%',
   };
+  const errMessageStyle = {
+    marginTop: '2%',
+    fontSize: 'small',
+    color: '#7c7c7c',
+  };
   return (
     <div style={errorStyle}>
       <h1>{props.title}</h1>
       <p>{props.content}</p>
+      <div style={errMessageStyle}>
+        만약 다른 기기에서는 접속이 되나요?
+        <div>
+          <a href="/state">이곳에서</a> 상태를 확인하세요!
+        </div>
+      </div>
     </div>
   );
 }

21c5e93208c17e603c92590bc56ef4ba8d6c1e90
kangdongmandoo <kangdongmandoo@gmail.com>
2021-05-14 15:16:23 +0900

    Update state.jsx

--- src/Pages/State/state.jsx
+++ src/Pages/State/state.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Marked, MarkedHTML } from '../../Library/markdown';
+import { nowTime } from '../../Library/time';
+import { utc } from '../../Library/utc';
 import './style.scss';
 
 let data = {};
-
+let t = Date.now();
 fetch(
   'https://api.netlify.com/api/v1/sites/f44e357a-0989-41b3-9bdc-f73f37499d6d'
 )
@@ -30,13 +32,20 @@ fetch(
     }
     if (document.getElementById('state-full')) {
       document.getElementById('state-full').innerHTML = Marked(
-        `\`\`\`json\n// General\n${JSON.stringify(
+        `\`\`\`json\n// Site\n${JSON.stringify(
           data
         )}\n// Deploy\n// Loading..\n\`\`\``
       );
     }
     console.log(data);
-
+    const st = Date.now();
+    if (document.getElementById('state-time-f')) {
+      document.getElementById(
+        'state-time-f'
+      ).innerHTML = `첫 번째 데이터 (site) : <code id="state-value">${
+        st - t
+      }ms</code>`;
+    }
     // console.log('[deploy_id] ' + data['deploy_id']);
     fetch(`https://api.netlify.com/api/v1/deploys/${data['deploy_id']}`)
       .then((ress) => {
@@ -56,21 +65,34 @@ fetch(
         }
         if (document.getElementById('state-full')) {
           document.getElementById('state-full').innerHTML = Marked(
-            `\`\`\`json\n// General\n${JSON.stringify(
+            `\`\`\`json\n// Site\n${JSON.stringify(
               data
             )}\n// Deploy\n${JSON.stringify(jsonn)}\n\`\`\``
           );
         }
+        if (document.getElementById('state-time')) {
+          document.getElementById(
+            'state-time'
+          ).innerHTML = `걸린 시간 : <code id="state-value">${
+            Date.now() - t
+          }ms</code>`;
+        }
+        if (document.getElementById('state-time-s')) {
+          document.getElementById(
+            'state-time-s'
+          ).innerHTML = `두 번째 데이터 (deploy) : <code id="state-value">${
+            Date.now() - st
+          }ms</code>`;
+        }
       });
   });
-
-const day = new Date();
 
 export default function App() {
   return (
     <div className="state-container">
       <h2># Site State</h2>
-      <p id="state-day">{`UTC ${day.getUTCFullYear()}년 ${day.getUTCMonth()}월 ${day.getUTCDay()}일 ${day.getUTCHours()}시 ${day.getUTCMinutes()}:${day.getUTCSeconds()}`}</p>
+      <p id="state-day">{utc()}</p>
+      <p id="state-day">{nowTime()}</p>
       <h3>State</h3>
       <p id="state">
         state : <code>Loading..</code>
@@ -93,9 +115,21 @@ export default function App() {
       <p
         id="state-full"
         dangerouslySetInnerHTML={MarkedHTML(
-          '```json\n// General\n// Loading..\n// Deploy\n// Loading..\n```'
+          '```json\n// Site\n// Loading..\n// Deploy\n// Loading..\n```'
         )}
       ></p>
+      <h3>ETC</h3>
+      <p id="state-time">
+        걸린 시간 : <code>Waiting..</code>
+      </p>
+      <h4>타임 스템프</h4>
+
+      <p id="state-time-f">
+        첫 번째 데이터 (site) : <code>Waiting..</code>
+      </p>
+      <p id="state-time-s">
+        두 번째 데이터 (deploy) : <code>Waiting..</code>
+      </p>
     </div>
   );
 }

4351bf58a20914b9fa5fb0f274258a650f261e27
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2021-07-06 07:53:24 +0900

    Update markdown.js

--- src/Library/markdown.js
+++ src/Library/markdown.js
@@ -3,9 +3,17 @@ import hljs from 'highlight.js';
 
 const renderer = new marked.Renderer();
 
+renderer.heading = function (text, level, id) {
+  const v = id.toLowerCase().replaceAll(' ', '-');
+  const r = `<h${level} id="${v}"><a href="#${v}" style="display: inline;" class="h-sharp">#</a>${text}</h${level}>\n`;
+  console.log(r);
+  return r;
+};
+
 marked.setOptions({
   langPrefix: 'hljs language-',
   renderer: renderer,
+  xhtml: true,
   highlight: function (code) {
     return hljs.highlightAuto(code, [
       'html',

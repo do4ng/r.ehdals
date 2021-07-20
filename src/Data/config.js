c6cd8c6c237227a8a99e7e984913b92209060cd3
kangdongmandoo <kangdongmandoo@gmail.com>
2021-07-20 23:42:45 +0900

    Update config.js

--- src/Data/config.js
+++ src/Data/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   nick: '강동민',
-  des: '게임과 컴퓨터를 좋아하는 학생입니다.',
+  des: '08년생 게임을 좋아하는 학생입니다.',
   email: 'kangdongmandoo@gmail.com',
   img: 'https://avatars.githubusercontent.com/u/57896501?v=4',
   github: 'https://github.com/kangdongmandoo',
